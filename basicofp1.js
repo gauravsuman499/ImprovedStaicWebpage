14ef9b1e94dc346ca2db9711cb76e82fe633dd27
gauravsuman499 <116598523+gauravsuman499@users.noreply.github.com>
2023-01-13 01:13:51 +0530

    Add files via upload

---
+++ basicofp1.js
@@ -0,0 +1,17 @@
+
+const basicOfP1 = (
+    <div>
+           <img src="images/testing.png"
+            height = "80px" width = "80px" />
+        <h1> Fun facts about React </h1>
+        <ul>
+            <li>Was first released in 2013</li>
+            <li>Was originally created by Jordan Walke</li>
+            <li>Has well over 100k stars over GitHub</li>
+            <li>Is maintained by facebook</li>
+            <li>powers thousand of enterprise app , including mobile apps</li>
+        </ul>
+    </div>
+)
+
+ReactDOM.render(basicOfP1 , document.getElementById("root"))
